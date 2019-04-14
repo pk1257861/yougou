a3780e05e5a4ad132403fae532fcffe98114a51f
pk1257861 <2823842330@qq.com>
2019-04-14 08:46:07 +0800

    优购一次

---
+++ src/pages/category/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>catrgory</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- 搜索 -->
+    <div class="search">
+      <div class="serach_input">
+        <input type="text">
+        <div class="word">
+          <icon type="search" size="20"></icon>
+          <span>搜索</span>
+        </div>
+      </div>
+    </div>
+    <!-- 轮播图  -->
+    <swiper class="loop" circular indicator-dots autoplay>
+      <block v-for="(item,index) in imgList" :key="index">
+        <swiper-item>
+          <image :src="item.image_src" />>
+        </swiper-item>
+      </block>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {   
+ data() {
+   return {
+     imgList:[],
+     a:1
+   }
+ },
+ mounted() {
+  wx.request({
+     url:"https://itjustfun.cn/api/public/v1/home/swiperdata",
+     data: { },
+  header: {
+    'content-type': 'application/json' // 默认值
+  },
+  success: (res)=>{
+   
+   this.imgList=res.data.data
+    console.log(res)
+  }
+   })
+ },
+};
+</script>
+
+<style lang=less>
+@import "./index.less";
+</style>
